6b6ca1a27653fd000ca8c2f68c29c7c0125f3082
Davepuffo <123974851+Davepuffo@users.noreply.github.com>
2023-03-29 17:50:16 +0200

    eserciziog3w9

--- esercizio/src/components/AllTheBooks.jsx
+++ esercizio/src/components/AllTheBooks.jsx
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
 import books from "../books/fantasy.json"
+import Button from 'react-bootstrap/Button';
 
 
 class AllTheBooks extends Component {
@@ -14,8 +15,15 @@ class AllTheBooks extends Component {
                     {books.map((book) => {
                         return (
                             <Col xs={2} className="mb-3">
-                                <Card className="bg-white text-white h-100 justify-content-center" key={book.asin}>
-                                    <Card.Img src={book.img} alt={book.title} />
+                                <Card className="h-100 d-flex justify-content-between" key={book.asin}>
+                                    <Card.Img variant="top" src={book.img} height="200px" />
+                                    <Card.Body>
+                                        <Card.Title>{book.title}</Card.Title>
+                                        <Card.Text>
+                                            {book.price}€
+                                        </Card.Text>
+                                        <Button variant="primary">Scopri di più</Button>
+                                    </Card.Body>
                                 </Card>
                             </Col>
                         )
